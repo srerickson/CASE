bbf12dd760781c9cb00bcc7b7d7d6ec992765a43
Seth <sr.erickson@gmail.com>
2012-06-29 17:14:18 -0700

    validate user evaluations to prevent duplicate evaluations by the same user (with the same eval question set)

--- app/models/user_evaluation.rb
+++ app/models/user_evaluation.rb
@@ -6,17 +6,34 @@ class UserEvaluation < ActiveRecord::Base
   has_many :evaluation_questions, :through => :evaluation_set
   has_many :user_evaluation_answers, :dependent => :destroy
   accepts_nested_attributes_for :user_evaluation_answers
-  validates_presence_of :user, :evaluation_set, :bird  
+  
+  validates_presence_of :user, :evaluation_set, :bird
+  validate :unique_user_evalset_bird_triple, :on => :create 
+  
   before_create :build_answer_records
   
+
+
+  def description
+    "#{evaluation_set.name}: #{bird.name}"
+  end
+
+  private
+
   def build_answer_records
+    logger.info(" --- build_answer_records  ----  ")
     self.evaluation_set.evaluation_questions.each do |q|
       self.user_evaluation_answers.build(:evaluation_question => q)
     end
   end
 
-  def description
-    "#{evaluation_set.name}: #{bird.name}"
+
+  def unique_user_evalset_bird_triple
+    logger.info(" --- unique_user_evalset_bird_triple  ----  ")
+
+    if UserEvaluation.where({:user_id => user_id, :evaluation_set_id => evaluation_set_id, :bird_id => bird_id }).count > 0
+       errors.add(:bird_id, "You have already evaluated '#{bird.name}' with evaluation set '#{evaluation_set.name}'")
+    end
   end
   
 end
