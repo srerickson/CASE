bc9c71169f1f7021584f9e0defc56d0b71ee1736
Seth <sr.erickson@gmail.com>
2012-09-05 12:04:39 -0700

    implementing locking/unlocking for evaluation sets

--- app/admin/evaluation_sets.rb
+++ app/admin/evaluation_sets.rb
@@ -8,23 +8,36 @@ ActiveAdmin.register EvaluationSet do
 
   index do 
     column :name
-    column :owner
     column :locked do |es|
       es.locked ? "Yes" : "No"
-    end
-    column "# user evaluations" do |es|
-      es.user_evaluations.count   
-    end    
+    end   
     column "# questions" do |es|
       es.evaluation_questions.count   
-    end    
-    default_actions
+    end
+    column "# birds evaluated" do |es|
+      es.birds.count.to_s +
+      " / " +
+      Bird.all.count.to_s
+    end 
+    column "# user evaluations" do |es|
+      es.user_evaluations.count   
+    end     
+    column "" do |es|
+      link_to "view", admin_evaluation_set_path(es)
+    end
   end
 
   show :title => :name do |es|
     attributes_table do
       row :name
       row :owner
+      row :locked do 
+        if es.locked
+          raw "This evaluations set is <b style='color:red'>locked</b>: it cannot be modified or deleted without first unlocking it." 
+        else
+          raw "This evaluations set is <b style='color:green'>unlocked</b>: It can be modified or deleted." 
+        end
+      end
       row :instructions
       table_for(es.evaluation_questions) do
           column :question do |q|
@@ -63,5 +76,53 @@ ActiveAdmin.register EvaluationSet do
     end
     f.buttons
   end
+
+
+
+
+  # Fix Action Buttons 
+  config.clear_action_items!
   
+
+  member_action :unlock, :method => :put do 
+    es = EvaluationSet.find(params[:id])
+    es.locked = false
+    es.save!
+    redirect_to admin_evaluation_set_path(es), :notice => "Unlocked!"
+  end
+
+  member_action :lock, :method => :put do 
+    es = EvaluationSet.find(params[:id])
+    es.locked = true
+    es.save!
+    redirect_to admin_evaluation_set_path(es), :notice => "Locked!"
+  end
+
+
+  action_item :only => :index do
+    link_to "New Evaluation Set", new_admin_evaluation_set_path
+  end  
+    
+  action_item :only => :show do
+    if !evaluation_set.locked
+      link_to "Edit", edit_admin_evaluation_set_path(evaluation_set)
+    end
+  end  
+
+  action_item :only => :edit do
+    link_to "Delete", admin_evaluation_set_path(evaluation_set),  :method => :delete, :confirm => "Are you READLLY SURE you want to delete this evaluation set?"
+  end  
+
+  action_item :only => :show do
+    if evaluation_set.locked
+      link_to unlock_admin_evaluation_set_path(evaluation_set), :method => :put do 
+        image_tag "unlock.png", :height=> "12"
+      end
+    else
+      link_to lock_admin_evaluation_set_path(evaluation_set),  :method => :put, do 
+        image_tag "lock.png", :height=> "12"
+      end
+    end
+  end 
+
 end
