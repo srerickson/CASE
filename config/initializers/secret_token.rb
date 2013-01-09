3ca70ef9babbaa2a9e63b77b036740bfff411019
Seth <sr.erickson@gmail.com>
2013-01-09 09:23:05 -0800

    remove secret_token.rb from public repo

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BoiCms20::Application.config.secret_token = '746d83f7406f0a6a3aed2d37385487964eb421429a757f09366b355fad67f94622590b2b182539d5081418ea1f628104b9ab533f1192ef1f40bd4be534b7c4d7'
